40507f642ba96c8aa1f90b48c135c3ec715644b5
derekangziying <derekangziying@gmail.com>
2020-08-20 03:59:41 +0800

    Updates

---
+++ precache-manifest.02f5a45cf1bfad120d830107d56ca2a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "88dd2298339154f30a81",
+    "url": "/create-react-app-github-pages/static/css/main.05e836f1.chunk.css"
+  },
+  {
+    "revision": "88dd2298339154f30a81",
+    "url": "/create-react-app-github-pages/static/js/main.fa104989.chunk.js"
+  },
+  {
+    "revision": "85da531c336290ac646a",
+    "url": "/create-react-app-github-pages/static/js/runtime~main.a93bb555.js"
+  },
+  {
+    "revision": "abc921c608ebbf2c207b",
+    "url": "/create-react-app-github-pages/static/js/2.fdc7f0fe.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/create-react-app-github-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "869526a8b2f94d1c09ebd3b82961eb53",
+    "url": "/create-react-app-github-pages/index.html"
+  }
+];
